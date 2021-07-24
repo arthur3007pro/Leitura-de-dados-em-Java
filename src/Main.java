963e2083d0f6a1fca126d6438c3777299c0250c3
Arthur Matheus <87915566+arthur3007pro@users.noreply.github.com>
2021-07-24 19:11:18 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class Main {
+
+		public static void main (String[] args) {
+			String product1 = "Computer";
+			String product2 = "Office desk";
+			
+			int age = 30;
+			int code = 5290;
+			char gender = 'F';
+			
+			double price1 = 2100.0;
+			double price2 = 650.50;
+			double measure = 53.234567;
+			
+			Locale.setDefault(Locale.US);
+			
+			System.out.println("Products: ");
+			System.out.printf("Computer which price is $ %.2f%n", price1);
+			System.out.printf("Office desk, wich price is $ %.2f%n", price2);
+			System.out.printf("record: 30 years old, code %d and gender: %s%n", code, gender);
+			
+			System.out.printf("Measue with eight decimal places: %f%n", measure);
+			System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+			System.out.printf("US decimal point: %.3f", measure);
+		}
+}
